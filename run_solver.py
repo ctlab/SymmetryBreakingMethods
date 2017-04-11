cb27b2cf765e5f039266c918af2c59a5086beb54
slava <slavam2605@bk.ru>
2017-04-11 19:16:55 +0300

    Handling of exit code of timeout and solver

--- run_solver.py
+++ run_solver.py
@@ -5,10 +5,14 @@ import time
 def run_solver(filename, max_seconds=3600, solver="treengeling"):
     cmd = ["timeout", str(max_seconds), "solvers/" + solver, filename]
     start = time.time()
-    if call(cmd):
+    ret_code = call(cmd)
+    # ret_code: 124 -- timeout
+    #           10  -- SAT
+    #           20  -- UNSAT
+    if ret_code == 124: 
         return None
     end = time.time()
-    return end - start
+    return end - start, ret_code == 10
 
 if __name__ == '__main__':
     print run_solver("models/0.dimacs")
