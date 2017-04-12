fc504b51aa6c389e83f73449755dc188a6e04e5e
Moklev Slava <slavam2605@bk.ru>
2017-04-12 11:55:13 +0300

    Tool for dumping test's results

--- test_suite.py
+++ test_suite.py
@@ -11,3 +11,25 @@ def test(n, m=None, bfs=False, max_deg=False, weights=False, degs=False,
     if sat == unsat:
         raise Exception("Model should be " + ("UNSAT" if unsat else "SAT") + ", but solver reports the opposite result")
     return time, sat
+
+
+def run_test(filename, n, m=None, bfs=False, max_deg=False, weights=False, degs=False,
+             lex=False, unavoid=False, unsat=False, max_seconds=3600, solver="treengeling"):
+    time, sat = test(n, m, bfs, max_deg, weights, degs, lex, unavoid, unsat, max_seconds, solver)
+    with open(filename, "a") as f:
+        f.write("[test]\n")
+        f.write("n = " + str(n) + "\n")
+        f.write("m = " + str(m) + "\n")
+        f.write("bfs = " + str(m) + "\n")
+        f.write("max_deg = " + str(max_deg) + "\n")
+        f.write("weights = " + str(weights) + "\n")
+        f.write("degs = " + str(degs) + "\n")
+        f.write("lex = " + str(lex) + "\n")
+        f.write("unavoid = " + str(unavoid) + "\n")
+        f.write("unsat = " + str(unsat) + "\n")
+        f.write("max_seconds = " + str(max_seconds) + "\n")
+        f.write("solver = " + str(solver) + "\n")
+        f.write("time = " + str(time) + "\n")
+        f.write("sat = " + str(sat) + "\n")
+        f.write("[end]")
+
