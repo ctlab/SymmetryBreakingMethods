5e339f9a6d4449b163f4eb63eeeaedf035eab90a
Slava Moklev <smoklev@sphinx.zoo>
2017-04-21 17:16:58 +0300

    Minor parser changes

--- result_parser.py
+++ result_parser.py
@@ -107,6 +107,8 @@ def q(s_elems, percent):
 
 def stat(elems):
     elems = list(map(lambda x: x[3] if x[6] is None else x[6], elems))
+    if len(elems) == 0:
+        return [None] * 4
     mean = sum(elems) / len(elems)
     s_elems = sorted(elems)
     return mean, q(s_elems, 0.5), q(s_elems, 0.25), q(s_elems, 0.75)
@@ -123,7 +125,7 @@ def print_table(res):
             elems = list(get_all(res, ident, n))
             sep = ' \\\\' if ident == idents[-1] else ' & '
             if len(elems) == 0:
-                print(' ' * 10 + sep, end='')
+                print(' ' * 11 + sep, end='')
             else:
                 tl = elems[0][3]
                 mean, median, q25, q75 = stat(elems)
@@ -139,11 +141,28 @@ def print_table_sat(filename):
     print_table(list(filter(lambda x: not x[2], parse_results(filename))))
 
 
+def print_pgfplot_data(filename, ident, unsat=False):
+    print('n t t25 t75')
+    elems = list(filter(lambda x: x[2] == unsat, parse_results(filename)))
+    min_n = min(map(lambda x: x[0], elems))
+    max_n = max(map(lambda x: x[0], elems))
+    for n in xrange(min_n, max_n + 1):
+        res = get_all(elems, ident, n)
+        mean, median, q25, q75 = stat(res)
+        #if unsat:
+        #    print(str(n) + ' ' + str(median if median else 3600))
+        #else:
+        print(str(n) + ' ' + str(median) + ' ' + str(median - q25) + ' ' + str(q75 - median))
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if len(sys.argv[1]) == 6:
-            for elem in get_all(filter(lambda x: not x[2], parse_results('dumps/dump1')), sys.argv[1], int(sys.argv[2])):
-                print(elem)
+            if len(sys.argv) == 2 or sys.argv[2] == 'unsat':
+                print_pgfplot_data('dumps/dump1', sys.argv[1], len(sys.argv) > 2)
+            else:
+                for elem in get_all(filter(lambda x: not x[2], parse_results('dumps/dump1')), sys.argv[1], int(sys.argv[2])):
+                    print(elem)
         elif sys.argv[1] == 'sat':
             print_table_sat('dumps/dump1')
         else:
